6c237a2c7a2b272d40e805f47033e84f9f982e01
Gabriel Strzal <gabrielstrzal@gmail.com>
2018-07-15 21:57:55 -0300

    added player animation

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -13,16 +13,21 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
 		$Sprite.flip_h = false
+		$Sprite.play("run")
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
 		$Sprite.flip_h = true
+		$Sprite.play("run")
 	else:
 		motion.x = 0
+		$Sprite.play("idle")
 	
 	if is_on_floor():
 		#motion.y = 0
 		if Input.is_action_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+	else:
+		$Sprite.play("jump")
 	
 	motion = move_and_slide(motion, UP)
 	
